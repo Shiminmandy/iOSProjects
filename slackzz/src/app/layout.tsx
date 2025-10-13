02fa4c9e6153d84e197a4addc1320dca53012b8e
Shiminmandy <66579256+Shiminmandy@users.noreply.github.com>
2025-10-12 23:41:00 -0400

    setup supabase

--- slackzz/src/app/layout.tsx
+++ slackzz/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Lato } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.className}  antialiased`}
       >
         {children}
       </body>
