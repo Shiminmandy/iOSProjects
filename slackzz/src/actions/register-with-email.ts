d92342ece50d5384ebd9e2c311b0c99720da0469
Shiminmandy <66579256+Shiminmandy@users.noreply.github.com>
2025-10-18 19:40:13 -0400

    auth setting

--- slackzz/src/actions/register-with-email.ts
+++ slackzz/src/actions/register-with-email.ts
@@ -3,7 +3,8 @@
 
 import { supabaseServerClient } from "@/supabase/supabaseServer";
 
-
+// 若邮箱存在且已验证，则按照无密码登录处理，发送magic link给用户
+// 若邮箱不存在或未验证，则创建新用户并发送confirm link给用户
 export async function registerWithEmail({email}: {email:string}) {
 
     const supabase = await supabaseServerClient();
@@ -12,8 +13,6 @@ export async function registerWithEmail({email}: {email:string}) {
     const {data, error} = await supabase.auth.signInWithOtp({
       email,
       options: {
-        // set this to false if you do not want the user to be automatically signed up
-        shouldCreateUser: true,
         emailRedirectTo: process.env.NEXT_PUBLIC_CURRENT_ORIGIN,
       },
     })
