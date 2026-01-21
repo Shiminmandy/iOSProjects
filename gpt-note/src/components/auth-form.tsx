00e123868eaf339bc8a0feebccd0c03a0469787f
Shiminmandy <66579256+Shiminmandy@users.noreply.github.com>
2026-01-21 12:06:53 -0800

    Add initial structure for AuthForm component: implement form type handling, integrate useRouter and useTransition hooks, and set up handleSubmit function for future form submission logic.

--- gpt-note/src/components/auth-form.tsx
+++ gpt-note/src/components/auth-form.tsx
@@ -1,8 +1,23 @@
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
 type Props = {
     type: "login" | "signUp";
   };
 
 export const AuthForm = ({ type }: Props) => {
+
+    const isLoginForm = type === "login";
+
+  const router = useRouter();
+
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = async (formData: FormData) => {
+    
+  }
+
     return (
         <div>
             <h1>{type}</h1>
